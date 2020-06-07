ea194af772468428295a23f57fcf2e465070a6b1
Igor Bolotnikov <igorbolotnikov1993@gmail.com>
2020-06-07 22:23:12 +0300

    Add main menu

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Render board with hint', () => {
   const { getByText } = render(<App />);
   const hintElement = getByText("Here's your board");
   expect(hintElement).toBeInTheDocument();
